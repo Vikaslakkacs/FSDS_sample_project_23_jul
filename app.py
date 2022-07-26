31a65526cc0a00402f74291aa90a50fd8d958896
Vikas Lakka <vikaslakkacs@gmail.com>
2022-07-26 18:22:31 +0530

    added code for logging and exception

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, app
-
+from housing.logger import logging
+from housing.exception import HousingException
+import sys
 app=Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 
 def index():
+    try:
+        raise Exception("Exception raised for testing")
+    except Exception as e:
+        housing= HousingException(e,sys)
+        logging.info("Exception testing")
+        logging.info(housing.error_message)
+        logging.info("Testing logging module.")
+        ##raise HousingException(e, sys) from e
     return "CI/CD pipe line has successfully completed"
 
 if __name__=="__main__":
---
+++ housing/exception/__init__.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+class HousingException(Exception):
+    
+    
+    def __init__(self, error_message:Exception, error_details:sys):
+        ## Pass information to parent class Exception
+        super().__init__(error_message)## == Exception(error_message)
+        self.error_message=HousingException.get_detailed_error_message(error_message=error_message,
+                                                                        error_details=error_details)
+
+    @staticmethod
+    def get_detailed_error_message(error_message:Exception, error_details:sys)-> str:
+        ### Returns tuple with traceback string
+        _, _, exeception_traceback= error_details.exc_info()
+        line_number= exeception_traceback.tb_frame.f_lineno
+        file_name= exeception_traceback.tb_frame.f_code.co_filename
+
+        error_message= f"Error occurred in script:[{file_name}] at line number: [{line_number}] error message: [{error_message}]"
+        return error_message
+    
+    ## Error message
+    def __str__(self):
+        return self.error_message
+    
+    def __repr__(self)-> str:
+        return HousingException.__name__.str()
