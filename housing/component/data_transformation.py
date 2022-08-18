fd24d2ee38b380174a4e028597a8f37714b836b4
Vikas Lakka <vikaslakkacs@gmail.com>
2022-08-18 16:42:20 +0530

    data transformation

--- housing/component/data_transformation.py
+++ housing/component/data_transformation.py
@@ -1,8 +1,82 @@
+from argparse import ONE_OR_MORE
+from msilib.schema import CreateFolder
 import os, sys
-from housing.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
+from ssl import OP_NO_RENEGOTIATION
+from housing.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact, DataTransformationArtifact
 from housing.logger import logging
 from housing.exception import HousingException
 from housing.entity.config_entity import DataTransformationConfig
+import numpy as np
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from housing.util.util import read_yaml_file, save_object, save_numpy_array_data, load_numpy_array_data
+from housing.constant import *
+
+
+
+
+class FeatureGenerator(BaseEstimator, TransformerMixin):
+    """Generation of New features for dataset
+
+    Args:
+        BaseEstimator (_type_): No args
+        TransformerMixin (_type_): No args
+    """
+
+    def __init__(self, add_bedrooms_per_room=True,
+                 total_rooms_ix=3,
+                 population_ix=5,
+                 households_ix=6,
+                 total_bedrooms_ix=4, columns=None):
+        """
+        FeatureGenerator Initialization
+        add_bedrooms_per_room: bool
+        total_rooms_ix: int index number of total rooms columns
+        population_ix: int index number of total population columns
+        households_ix: int index number of  households columns
+        total_bedrooms_ix: int index number of bedrooms columns
+        """
+        try:
+            self.columns = columns
+            if self.columns is not None:
+                total_rooms_ix = self.columns.index(COLUMN_TOTAL_ROOMS)
+                population_ix = self.columns.index(COLUMN_POPULATION)
+                households_ix = self.columns.index(COLUMN_HOUSEHOLDS)
+                total_bedrooms_ix = self.columns.index(COLUMN_TOTAL_BEDROOM)
+
+            self.add_bedrooms_per_room = add_bedrooms_per_room
+            self.total_rooms_ix = total_rooms_ix
+            self.population_ix = population_ix
+            self.households_ix = households_ix
+            self.total_bedrooms_ix = total_bedrooms_ix
+        except Exception as e:
+            raise HousingException(e, sys) from e
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        try:
+            room_per_household = X[:, self.total_rooms_ix] / \
+                                 X[:, self.households_ix]
+            population_per_household = X[:, self.population_ix] / \
+                                       X[:, self.households_ix]
+            if self.add_bedrooms_per_room:
+                bedrooms_per_room = X[:, self.total_bedrooms_ix] / \
+                                    X[:, self.total_rooms_ix]
+                generated_feature = np.c_[
+                    X, room_per_household, population_per_household, bedrooms_per_room]
+            else:
+                generated_feature = np.c_[
+                    X, room_per_household, population_per_household]
+
+            return generated_feature
+        except Exception as e:
+            raise HousingException(e, sys) from e
 
 
 class DataTransformation:
@@ -15,5 +89,94 @@ class DataTransformation:
             self.data_transformation_config= data_transformation_config
             self.data_ingestion_artifact= data_ingestion_artifact
             self.data_validation_artifact= data_validation_artifact
+        except Exception as e:
+            raise HousingException(e, sys) from e
+
+    @staticmethod
+    def load_data(file_path:str, schema_file_path:str) -> pd.DataFrame:
+        """Changing the type cast of the features according to schema file
+
+        Args:
+            file_path (str): _description_
+            schema_file_path (str): schema file path where all the type cast details of features are available
+
+        Returns:
+            pd.DataFrame: dataset with corrected type cast of features to make transformation
+        """
+        try:
+            dataset_schema= read_yaml_file(schema_file_path)
+            schema= dataset_schema[DATASET_SCHEMA_COLUMNS_KEY]
+
+            ##Get the data frame
+            dataframe= pd.read_csv(file_path)
+
+            error_message= ""
+
+            ### loop around the columns and change the data types
+            for column in dataframe.columns:
+                if column in list(schema.keys()):
+
+                    dataframe[column].astype(schema[column])
+                else:
+                    error_message= f"{error_message} \nColumn: {column} is not in schema."
+                    raise Exception(error_message)
+            return dataframe
+            
+        except Exception as e:
+            raise HousingException(e, sys) from e
+
+    def get_transformer_object(self)->ColumnTransformer:
+        """Creating pipeline which transformes both numerical and categorical objects
+
+        Raises:
+            HousingException: Column transformation of pipeline cannot be done
+
+        Returns:
+            ColumnTransformer: ColumnTransformer which consists of pipelines
+        """
+
+        try:
+            schema_file_path= self.data_validation_artifact.schema_file_path
+
+            dataset_schema=read_yaml_file(schema_file_path)
+            numerical_columns=dataset_schema[NUMERICAL_COLUMN_KEY]
+            categorical_columns= dataset_schema[CATEGOTICAL_COLUMN_KEY]
+
+            num_pipeline= Pipeline(steps=[
+                                    ('imputer', SimpleImputer(strategy='median')),
+                                    ('feature_generator', FeatureGenerator(
+                                        add_bedrooms_per_room=self.data_transformation_config.add_bedroom_per_room,
+                                        columns=numerical_columns)),
+                                    ('scaler', StandardScaler())
+                                         ]
+                                  )
+            logging.info(f"Pipeline has been created for numerical columns:{numerical_columns}")
+            cat_pipeline= Pipeline(steps=[
+                                    ('impute', SimpleImputer(strategy='most_frequent')),
+                                    ('one_hot_encoding', OneHotEncoder()),
+                                    ('scaler', StandardScaler(with_mean=False))
+                                         ]
+                                  )
+            logging.info(f"Pipeline has been created for categorical columns{categorical_columns}")
+
+            ## Preprocessing steps
+            preprocessing= ColumnTransformer([
+                                                ('numerical_pipeline', num_pipeline, numerical_columns),
+                                                ('categorical_pipeline', cat_pipeline, categorical_columns)
+                                             ]
+                                            )
+            
+            return preprocessing
+
+        except Exception as e:
+            raise HousingException(e, sys) from e
+    def initiate_data_transformation(self, )->DataTransformationArtifact:
+        """Transformation of the data is performed by considering pipelines and dataset
+
+        Returns:
+            DataTransformationArtifact: Data Transformation artifact details
+        """
+        try:
+            preprocessing_obj= self.get_transformer_object
         except Exception as e:
             raise HousingException(e, sys) from e
--- housing/util/util.py
+++ housing/util/util.py
@@ -1,7 +1,9 @@
 import os, sys
+from tkinter import E
 import yaml
 from housing.exception import HousingException
-
+import numpy as np
+import dill
 
 def read_yaml_file(file_path:str)->dict:
     """reads YAML file with package yaml
@@ -16,5 +18,68 @@ def read_yaml_file(file_path:str)->dict:
         with open(file_path,'rb') as yaml_file:
             return yaml.safe_load(yaml_file)
 
+    except Exception as e:
+        raise HousingException(e, sys) from e
+
+
+def save_numpy_array_data(file_path:str, array:np.array):
+    """Saves Numpy array data to file
+
+    Args:
+        file_path (str): location to store the file
+        array (np.array): array data to save
+    """
+    try:
+        dir_path=os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
+            np.save(file_obj, array)
+
+    except Exception as e:
+        raise HousingException(e, sys) from e
+
+def load_numpy_array_data(file_path:str) -> np.array:
+    """Load Numpy array data from file
+
+    Args:
+        file_path (str): file path where file object is stored
+
+    Raises:
+        HousingException: Loading un successfull
+
+    Returns:
+        np.array: Numpy array of loaded data
+    """
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return np.load(file_obj)
+        
+    except Exception as e:
+        raise HousingException(e, sys) from e
+
+def save_object(file_path:str, obj):
+    """Saving object using dill library
+
+    Args:
+        file_name (str): string
+        obj (_type_): any object
+    """
+    try:
+        dir_path= os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+    except Exception as e:
+        raise HousingException(e, sys) from e
+
+def load_object(file_path:str):
+    """Loading file object from file to system
+
+    Args:
+        file_path (str): file path where object is stored
+    """
+    try:
+        with open(file_path, 'rb') as file_obj:
+            dill.load(file_obj)
     except Exception as e:
         raise HousingException(e, sys) from e
